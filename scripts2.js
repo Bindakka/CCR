8ae42d178bfbdca8a53b0aa9fe70fd988be9bd3c
Bindu R N <145004213+Bindakka@users.noreply.github.com>
2024-08-16 09:23:52 +0530

    Add files via upload

---
+++ scripts2.js
@@ -0,0 +1,9 @@
+// Add any custom JavaScript here
+
+$(document).ready(function() {
+    // Example of form submission using AJAX
+    $('#contact-form').on('submit', function(event) {
+        event.preventDefault();
+        alert('Form submitted! We will get back to you soon.');
+    });
+});
